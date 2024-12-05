86e20fe1f11b0d15ce031c71bd940df7b25db57b
selimrachdi <selim.rachdi@esprit.tn>
2024-12-05 10:15:33 +0100

    formatting update

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,13 +1,13 @@
 """
-calculator functions
+Calculator functions
 """
 
 
-def add(x,y):
-  """Add x and y and return result."""
-  return x+y
+def add(x, y):
+    """Add x and y and return the result."""
+    return x + y
 
 
-def substract(x,y):
-  """Substract x from y and return result."""
-  return y-x
+def subtract(x, y):
+    """Subtract y from x and return the result."""
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,18 +1,19 @@
 """
-Sammple tests
+Sample tests
 """
 from django.test import SimpleTestCase
 from app import calc
 
 
 class CalcTests(SimpleTestCase):
-  """Test the calc module"""
-  def test_add_numbers(self):
-    """"test adding numbers together."""
-    res = calc.add(5,6)
+    """Test the calc module"""
 
-    self.assertEqual(res,11)
-  def test_substract_numbers(self):
-    """test substract numbers."""
-    res=calc.substract(10,15)
-    self.assertEqual(res,5)
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(15, 10)
+        self.assertEqual(res, 5)
